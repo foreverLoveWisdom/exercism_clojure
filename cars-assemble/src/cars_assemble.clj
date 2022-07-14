16f91a5995e86a89db833f57a4846609b59c8d96
foreverLoveWisdom <forever.love.wisdom@gmail.com>
2022-07-14 22:59:33 +0700

    Why: Basic Clojure

---
+++ cars-assemble/src/cars_assemble.clj
@@ -0,0 +1,23 @@
+(ns cars-assemble)
+
+(defn success-rate [speed]
+  (cond
+       (= 0 speed) 0.0
+       (<= 1 speed 4) 1.0
+       (<= 5 speed 8) 0.9
+       (= 9 speed) 0.8
+       (= 10 speed) 0.77))
+
+
+(defn production-rate
+  "Returns the assembly line's production rate per hour,
+   taking into account its success rate"
+  [speed]
+  (* 221 speed (success-rate speed)))
+
+(defn working-items
+  "Calculates how many working cars are produced per minute"
+  [speed]
+  (int (/ (production-rate speed) 60)))
+
+
